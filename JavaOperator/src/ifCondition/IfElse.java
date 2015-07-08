6526d7f312218552c4c6796bd3015ede94c4c1ed
itbank <itbank@itbank-PC>
2015-07-08 15:18:59 +0900

    2015.07.08 3일차 종료. if-else

--- JavaOperator/src/ifCondition/IfElse.java
+++ JavaOperator/src/ifCondition/IfElse.java
@@ -1,6 +1,6 @@
 package ifCondition;
 
-public class OnlyIf {
+public class IfElse {
 
 	public static void main(String[] args) {
 		
@@ -8,9 +8,11 @@ public class OnlyIf {
 		if ((x==1) && (y==1)){ // &&(앤퍼센트)라고 읽고 and연산자이다. &&는 둘다 참이어야 출력됨
 			System.out.println("x는 1이면서 y도 2이다.");
 		}
-		if ((x==1) || (y==1)) { // ||(컬럼)이라고 읽고 or연산자이다. ||는 하나만 참이어도 출력됨
+		else if ((x==1) || (y==1)) { // ||(컬럼)이라고 읽고 or연산자이다. ||는 하나만 참이어도 출력됨
 			System.out.println("x는 1이거나 y는 2이다.");
-			
+		}
+		else {
+			System.out.println("해당사항이 없습니다.");
 		}
 
 	}
